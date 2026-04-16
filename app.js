ac7c369745b5e71671e7639f9e0d47f039a7e759
Sebastian Cielachowski <topsc558@gmail.coom>
2026-04-16 22:36:41 +0200

    feat: add CSS styles, JS smooth scroll and complete page sections

---
+++ app.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        document.querySelector(link.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+    });
+});
